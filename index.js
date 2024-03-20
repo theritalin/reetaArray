cf45c0e6096e0956f3bdd49ab9371eaa869423aa
theritalin <fatihbasacek@gmail.com>
2024-03-20 12:30:43 +0300

    version upfate

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 const reeta = require("reeta");
-
-
+const chalk = require("chalk");
 
 function existInArray(array, value) {
   var len = array.length;
 
   for (var i = 0; i < len; i++) {
     if (array[i] === value) {
+      console.log(chalk.green("It exists"));
       return true;
+    } else {
+      console.log(chalk.red("It doesn't exists"));
+      return false;
     }
   }
-  return false;
 }
 
 function unique(array) {
   reeta.uniqueValues(array);
 }
-
-
 
 module.exports = {
   existInArray,
